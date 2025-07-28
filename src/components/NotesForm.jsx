33bf77849e568d5a53e3771cc7d4e27c9fe30c28
nmasters <nmasters52@gmail.com>
2025-07-28 16:55:22 -0400

    add inputs and labels

--- src/components/NotesForm.jsx
+++ src/components/NotesForm.jsx
@@ -16,17 +16,54 @@ const NotesForm = () => {
         })
     }
 
-    console.table(formData)
-
   return (
     <form action="">
-        <label htmlFor="">Title</label>
-        <input 
-            name='title'
-            type="text" 
-            value={formData.title}
-            onChange={(e) => handleChange(e)}
-        />
+        <div className="mb-4">
+            <label htmlFor="" className="block font-semibold">Title</label>
+            <input 
+                name='title'
+                type="text"
+                className="w-full border-2 border-black rounded-sm"
+                value={formData.title}
+                onChange={(e) => handleChange(e)}
+            />
+        </div>
+        <div className="mb-4">
+            <label htmlFor="" className="block font-semibold">Priority</label>
+            <select 
+                name='priority'
+                className="w-full border-2 border-black rounded-sm"
+                value={formData.priority}
+                onChange={(e) => handleChange(e)}
+            > 
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+            </select>
+        </div>
+        <div className="mb-4">
+            <label htmlFor="" className="block font-semibold">Category</label>
+            <select
+                name='category'
+                className="w-full border-2 border-black rounded-sm"
+                value={formData.category}
+                onChange={(e) => handleChange(e)}
+            > 
+            <option value="Personal">Personal</option>
+            <option value="Work">Work</option>
+            <option value="Ideas">Ideas</option>
+            </select>
+        </div>
+        <div className="mb-4">
+            <label htmlFor="" className="block font-semibold">Description</label>
+            <textarea
+                name="description"
+                className="w-full border-2 border-black rounded-sm"
+                onChange={handleChange}
+            >
+            </textarea>
+        </div>
+        
     </form>
   )
 }
